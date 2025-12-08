9e158e2a24075adb1c4dcb5b3a4f7d70825359d9
atobys@mail.ru <your_email@abc.com>
2025-12-08 16:51:00 +0300

    Initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import './Navbar.css';
+import logo from '../logo.png';
+
+const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        <img src={logo} alt="Цифровая Аттенка" />
+      </div>
+      
+      <button className="menu-toggle" onClick={() => setMenuOpen(!menuOpen)}>
+        ☰
+      </button>
+
+      <ul className={`menu ${menuOpen ? 'active' : ''}`}>
+        <li><a href="#">Главная</a></li>
+        <li><a href="#">Записаться к врачу</a></li>
+        <li><a href="#">Каталог</a></li>
+        <li><a href="#">Мои тесты</a></li>
+      </ul>
+
+      <div className="language">
+        РУС <span className="dropdown-arrow">▼</span>
+        <button className="profile-button">Профиль</button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
